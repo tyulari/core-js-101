b4145ff0fe4ae7c6f5ebc2ce999562cc37a35c6f
tyulari <128141264+tyulari@users.noreply.github.com>
2023-10-23 09:50:52 +0300

    feat: finish string tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -175,8 +175,8 @@ function convertToUpperCase(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
@@ -202,8 +202,14 @@ function extractEmails(/* str */) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  const head = `${'┌'}${'─'.repeat(width - 2)}${'┐\n'}`;
+
+  const body = `${'│'}${' '.repeat(width - 2)}${'│\n'}`;
+
+  const footer = `${'└'}${'─'.repeat(width - 2)}${'┘\n'}`;
+
+  return head + body.repeat(height - 2) + footer;
 }
 
 
@@ -223,8 +229,18 @@ function getRectangleString(/* width, height */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const map = 'NOPQRSTUVWXYZABCDEFGHIJKLM[\\]^_`nopqrstuvwxyzabcdefghijklm';
+  const result = [];
+  for (let i = 0; i < str.length; i += 1) {
+    const symbcode = str[i].charCodeAt(0);
+    if (symbcode < 65 || symbcode > 122) {
+      result.push(str[i]);
+    } else {
+      result.push(map[symbcode - 65]);
+    }
+  }
+  return result.join('');
 }
 
 /**
@@ -240,8 +256,8 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 
@@ -269,8 +285,13 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const initialDesk = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+
+  return initialDesk.indexOf(value);
 }
 
 
